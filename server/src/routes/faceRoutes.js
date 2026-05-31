d47984ec3754ccfc2addb7a31ff78e83d56456c7
Rakhi3008-dev <rakhi_j@es.iitr.ac.in>
2026-06-01 02:23:25 +0530

    Add download button

--- server/src/routes/faceRoutes.js
+++ server/src/routes/faceRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import upload from "../config/multer.js";
+import { protect } from "../middleware/authMiddleware.js";
 import { uploadSelfie } from "../controllers/faceController.js";
 
 const router = express.Router();
 
 router.post(
-    "/upload-selfie",
-    protect,
-    upload.single("selfie"),
-    uploadSelfie
-  );
+  "/upload-selfie",
+  protect,
+  upload.single("selfie"),
+  uploadSelfie
+);
+
 export default router;
