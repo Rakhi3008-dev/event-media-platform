450cc2daba3629e13b487d001897b9f8f8b04ccd
Rakhi3008-dev <rakhi_j@es.iitr.ac.in>
2026-06-01 18:38:47 +0530

    Add AWS face matching backend

--- server/src/routes/faceRoutes.js
+++ server/src/routes/faceRoutes.js
@@ -3,48 +3,33 @@ import upload from "../config/multer.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { DetectFacesCommand } from "@aws-sdk/client-rekognition";
 import rekognition from "../config/rekognition.js";
-  import {
-    uploadSelfie,
-    getMySelfie,
-    getMyMatches
-  } from "../controllers/faceController.js";
+import {
+  uploadSelfie,
+  getMySelfie,
+  getMyMatches,
+} from "../controllers/faceController.js";
 const router = express.Router();
 
+router.post("/upload-selfie", protect, upload.single("selfie"), uploadSelfie);
+router.get("/my-selfie", protect, getMySelfie);
+router.get("/matches", protect, getMyMatches);
+router.get("/test-face", async (req, res) => {
+  try {
+    const command = new DetectFacesCommand({
+      Image: {
+        Bytes: Buffer.from("test"),
+      },
+    });
 
-router.post(
-    "/upload-selfie",
-    protect,
-    upload.single("selfie"),
-    uploadSelfie
-  );
-  router.get(
-    "/my-selfie",
-    protect,
-    getMySelfie
-  );
-  router.get(
-    "/matches",
-    protect,
-    getMyMatches
-  );
-  router.get("/test-face", async (req, res) => {
-    try {
-      const command = new DetectFacesCommand({
-        Image: {
-          Bytes: Buffer.from("test"),
-        },
-      });
-  
-      await rekognition.send(command);
-  
-      res.json({
-        success: true,
-      });
-  
-    } catch (err) {
-      res.json({
-        message: err.message,
-      });
-    }
-  });
+    await rekognition.send(command);
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    res.json({
+      message: err.message,
+    });
+  }
+});
 export default router;
